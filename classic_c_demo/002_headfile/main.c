cd4e892fb2a71f21d26f9bfc5cdb4755f052669e
023 <2633622312@qq.com>
2020-02-16 14:22:51 +0800

    commit

---
+++ classic_c_demo/002_headfile/main.c
@@ -0,0 +1,7 @@
+#include "print.h"
+
+int main()
+{
+    printHello();
+    return 0;
+}
---
+++ classic_c_demo/004_compile_two_numble.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float x,y,c;
+    printf("please input x and y:\n");
+    scanf("%f%f",&x,&y);
+    c = x>y?x:y;
+    printf("Max of (%f,%f) is %f",x,y,c);
+    return 0;
+}
